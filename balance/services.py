fd0de1d07721077b23e033d8379561c6e89d6cee
ivan_masiuk <vavolan@gmail.com>
2023-01-11 16:01:36 +0200

    impl: add services.py

---
+++ balance/services.py
@@ -0,0 +1,12 @@
+def get_user_ip(request):
+    """
+    Get IP user by request
+    :param request: request
+    :return: IP
+    """
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(",")[0]
+    else:
+        ip = request.META.get("REMOTE_ADDR")
+    return ip
--- balance/views.py
+++ balance/views.py
@@ -33,13 +33,6 @@ class TransactionCreateView(APIView):
     """
     Create transaction
     """
-    def get_user_ip(self, request):
-        x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
-        if x_forwarded_for:
-            ip = x_forwarded_for.split(",")[0]
-        else:
-            ip = request.META.get("REMOTE_ADDR")
-        return ip
 
     def post(self, request):
         transaction = TransactionCreateSerializer(data=request.data)

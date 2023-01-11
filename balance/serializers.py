7780428f2ed9bd6df7fbe3a3fa530e0536863168
ivan_masiuk <vavolan@gmail.com>
2023-01-11 13:38:58 +0200

    impl: add serializers.pyI

---
+++ balance/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from .models import Transaction
+
+
+class TransactionListSerializer(serializers.ModelSerializer):
+    """
+    Prepare data of transactions by fields
+    """
+
+    class Meta:
+        model = Transaction
+        fields = ("amount", "type", )
+
+
+class TransactionDetailsSerializer(serializers.ModelSerializer):
+    """
+    Prepare data of transaction by all fields
+    """
+
+    # user_fk = serializers.SlugRelatedField(
+    #     slug_field="name", read_only=True,  # many=True
+    # )
+
+    class Meta:
+        model = Transaction
+        exclude = ("updated", )
+
